0c57fdf77df51b8eac18cd12d9deac3c8837c927
aman123 <amanuelbizuayehu123@gmail.com>
2025-02-06 14:36:15 +0300

    merging and country code of the ip address

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 
 
 class DataPreprocessing:
-    def __init__(self, data):
+    def __init__(self):
+        pass
+    def to_date(self,column_list,data):
         self.data = data
-    def to_date(self,column_list):
         for column_name in column_list:
             self.data[column_name] = pd.to_datetime(self.data[column_name])
         return self.data
@@ -23,3 +24,17 @@ class DataPreprocessing:
 
         plt.tight_layout()
         return plt
+    
+    def assign_country_code(self,df_Fraud, df_IP):
+        country_list = []  # Store results
+
+        for fraud_ip in df_Fraud['ip_address']:
+            # Filter the correct country based on IP range
+            match = df_IP.loc[(df_IP['lower_bound_ip_address'] <= fraud_ip) & 
+                            (df_IP['upper_bound_ip_address'] >= fraud_ip), 'country']
+
+            # Append the first matched country (if any), otherwise append None
+            country_list.append(match.iloc[0] if not match.empty else None)
+
+        df_Fraud['country'] = country_list  # Add new column to df_Fraud
+        return df_Fraud
